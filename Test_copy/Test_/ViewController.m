cc9c4f576175d5b3b2011de50834e2605aa76e49
任超 <renchao@17shihui.com>
2015-08-30 12:50:12 +0800

    commit code

--- Test_copy/Test_/ViewController.m
+++ Test_copy/Test_/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"Test_copy file commit");
     NSLog(@"Hello World");
 }
 
