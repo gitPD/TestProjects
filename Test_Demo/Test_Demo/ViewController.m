3fa549b4b1e9f0bc3e1621cf6760446ce297a317
任超 <renchao@17shihui.com>
2016-03-22 17:57:51 +0800

    add new project

---
+++ Test_Demo/Test_Demo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Test_Demo
+//
+//  Created by rong on 16/3/22.
+//  Copyright © 2016年 rc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"hello word!");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Test_Demo/Test_Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test_Demo
+//
+//  Created by rong on 16/3/22.
+//  Copyright © 2016年 rc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
